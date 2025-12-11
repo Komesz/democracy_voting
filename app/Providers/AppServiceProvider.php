29d0bf56ef64f5058d89ae1333f4cc2be9914fd7
Komjáti Zsolt <admin@komesz.com>
2025-12-11 19:55:45 +0100

    FIX - https url for links.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Party;
 use App\Observers\PartyObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Party::observe(PartyObserver::class);
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
